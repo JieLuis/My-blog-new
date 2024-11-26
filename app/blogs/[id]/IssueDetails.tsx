b9d86b292441297dccc040e766be57a9dbb31faf
JieLuis <lionliao1658@gmail.com>
2024-11-26 10:04:41 +0800

    Feature: convert react-markdown to markdown-it

--- app/blogs/[id]/IssueDetails.tsx
+++ app/blogs/[id]/IssueDetails.tsx
@@ -1,10 +1,24 @@
 import { IssueStatusBadge } from "@/app/components";
 import { Issue, Prisma } from "@prisma/client";
 import { Box, Heading, Flex, Card, Text } from "@radix-ui/themes";
-import React from "react";
-import ReactMarkdown from "react-markdown";
+import React from "react"
+import MarkdownIt from "markdown-it"
+import styles from "@/app/posts/post.module.css"
+import matter from "gray-matter";
+
+interface ArticleHeader {
+  title: string,
+  date: string,
+  author: string
+}
+
+const md = new MarkdownIt()
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
+  const { data, content } = matter(issue.description)
+  const header = data as ArticleHeader
+  const htmlContent = md.render(content)
+
   return (
     <>
       <Heading>{issue.title}</Heading>
@@ -13,7 +27,14 @@ const IssueDetails = ({ issue }: { issue: Issue }) => {
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
       <Card className="prose max-w-full">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
+        <article className={styles.article}>
+          <h1 className="">{header.title}</h1>
+          <p className="post-meta">{header.date}</p>
+          <div
+          className="post-content"
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+          />
+        </article>
       </Card>
     </>
   );
